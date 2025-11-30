83a09b7f2909e1aa2230785b535f90df72ec8723
MohamadAlaskari <m_alaskari96@hotmail.com>
2025-12-01 00:28:55 +0200

    feat(analytics): integrate Vercel Web Analytics in frontend
    - Installed @vercel/analytics package
    - Added <Analytics /> component to main layout

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,6 +1,8 @@
 import type { ReactNode } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Analytics } from '@vercel/analytics/next';
+
 
 
 interface LayoutProps{
@@ -15,6 +17,7 @@ const Layout =({children, className}:LayoutProps)=>{
             <div className="pb-14">
             <main className={className}>
                 {children}
+                <Analytics />
             </main>
             </div>
             <Footer/>
